01ccafa5590fa76c93fe046b250244184b18cedd
Tekeste G <iamtekeste@gmail.com>
2016-06-12 16:24:20 -0500

    change page title based on current page

--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 const projects = require('./routes/projects');
+const pages = require('./routes/pages');
 
 mongoose.connect('mongodb://localhost/portfolio');
 
@@ -12,7 +13,15 @@ mongoose.connect('mongodb://localhost/portfolio');
 var hbs = exphbs.create({
 	defaultLayout: 'main',
 	layoutsDir: "app/views/layouts",
-	partialsDir: ['app/views/partials']
+	partialsDir: ['app/views/partials'],
+	menu: ['Projects'],
+	helpers: {
+	    section: function(name, options){
+	        if(!this._sections) this._sections = {};
+	        this._sections[name] = options.fn(this);
+	        return null;
+	    }
+	}
 });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -21,8 +30,10 @@ app.set('views', path.join(__dirname, '/views'));
 /** -- End --  **/
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
+
+app.use('/', pages);
+
 app.use('/projects', projects);
-app.use('/', projects);
 
 app.listen(3000, () => {
 	console.log('listening on http://localhost:3000');
